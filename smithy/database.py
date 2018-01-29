608c74cd9fd25338689fc7c0a4fa74b220e6cd71
Gareth Coles <gdude2002@gmail.com>
2018-01-29 13:56:59 +0000

    Move to peewee-db-evolve and a ManyToMany field

--- smithy/database.py
+++ smithy/database.py
@@ -1,13 +1,12 @@
 # coding=utf-8
 import asyncio
 
+import peeweedbevolve
 import peewee
 
 from peewee_async import Manager
 from peewee_asyncext import PooledPostgresqlExtDatabase
-from playhouse.postgres_ext import ArrayField
-from psycopg2._psycopg import AsIs
-from psycopg2.extensions import register_adapter
+from playhouse.fields import ManyToManyField
 
 from smithy.config import DATABASE
 
@@ -24,10 +23,10 @@ class Module(peewee.Model):
 
 
 class DBServer(peewee.Model):
-    server_id = peewee.IntegerField()
+    server_id = peewee.IntegerField(unique=True)
     bot_present = peewee.BooleanField(default=False)
     command_prefix = peewee.CharField(default="!")
-    modules = ArrayField(lambda: peewee.ForeignKeyField(Module, to_field=Module.id))
+    modules = ManyToManyField(Module, related_name="users")
     info_channel = peewee.IntegerField(null=True)
     notes_channel = peewee.IntegerField(null=True)
 
@@ -35,24 +34,11 @@ class DBServer(peewee.Model):
         database = database
 
 
-def create_tables() -> None:
-    DBServer.create_table(fail_silently=True)
-    Module.create_table(fail_silently=True)
-
-
-def adapt_model(model):
-    return AsIs("%s" % model.id)
-
-
-register_adapter(Module, adapt_model)
-
-
-if __name__ == "__main__":
-    async def test():
-        create_tables()
-        module = await manager.create_or_get(Module, id=1, name="test")
-        server = await manager.create_or_get(DBServer, server_id=0, modules=[module])
-        print(server)
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(test())
+def evolve() -> None:
+    database.evolve(
+        [
+            DBServer,
+            DBServer.modules.get_through_model(),
+            Module,
+        ]
+    )
