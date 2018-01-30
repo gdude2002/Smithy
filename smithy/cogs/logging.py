64cfecacc056f687e13e998108f246a5ca516f9d
Gareth Coles <gdude2002@gmail.com>
2018-01-30 14:57:17 +0000

    Better logging

--- smithy/cogs/logging.py
+++ smithy/cogs/logging.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from discord import Message, ChannelType, DMChannel
+from discord import Message
 from discord.ext.commands import Bot
 
 __author__ = "Gareth Coles"
@@ -15,14 +15,14 @@ class Logging:
     async def on_message(self, message: Message):
         if not message.guild:  # It's a DM
             print(
-                f"@{self.bot.user.name} | "
-                f"{message.author.name}#{message.author.discriminator} -> "
+                f"@{self.bot.user.name}#{self.bot.user.discriminator} | "
+                f"@{message.author.name}#{message.author.discriminator} -> "
                 f"{message.clean_content}"
             )
         else:
             print(
-                f"{message.guild.name} @ #{message.channel.name} | "
-                f"{message.author.name}#{message.author.discriminator} -> "
+                f"{message.guild.name} | #{message.channel.name} | "
+                f"@{message.author.name}#{message.author.discriminator} -> "
                 f"{message.clean_content}"
             )
 
